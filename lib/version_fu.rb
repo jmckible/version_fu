b40fd37dfaf2da4ff655049a4c43cf5c54dffd2a
Jordan McKible <jordan@mckible.com>
2008-06-03 23:19:03 -0700

    Spelling mistake on version_column config option.

--- lib/version_fu.rb
+++ lib/version_fu.rb
@@ -15,7 +15,7 @@ module VersionFu
       self.versioned_foreign_key        = options[:foreign_key] || self.to_s.foreign_key
       self.versioned_table_name         = options[:table_name]  || "#{table_name_prefix}#{base_class.name.demodulize.underscore}_versions#{table_name_suffix}"
       self.versioned_inheritance_column = options[:inheritance_column] || "versioned_#{inheritance_column}"
-      self.version_column               = options[:version_columne]    || 'version'
+      self.version_column               = options[:version_column]    || 'version'
 
       # Setup versions association
       class_eval do
