76e7290e61b0fb282d8ec0ca895a8c7b11782408
Jordan McKible <jordan@mckible.com>
2009-08-25 23:34:31 -0700

    Move database.yml and schema.rb to test/db

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,12 +5,12 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/e
 require 'active_record'
 require 'active_record/fixtures'
 
-config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
+config = YAML::load(IO.read(File.dirname(__FILE__) + '/db/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
 ActiveRecord::Base.configurations = {'test' => config[ENV['DB'] || 'sqlite3']}
 ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations['test'])
 
-load(File.dirname(__FILE__) + '/schema.rb')
+load(File.dirname(__FILE__) + '/db/schema.rb')
 
 require File.join(File.dirname(__FILE__), 'fixtures/author')
 require File.join(File.dirname(__FILE__), 'fixtures/page')
