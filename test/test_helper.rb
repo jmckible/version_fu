33f3fee917fa3116035727bf4f9533cca33dd50b
Herval Freire <hervalfreire@gmail.com>
2009-09-04 10:59:55 -0300

    fixed test run (now they run with 'rake'!)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 require 'test/unit'
 require 'rubygems'
-require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
 
 require 'active_record'
 require 'active_record/fixtures'
+require File.expand_path(File.join(File.dirname(__FILE__), '../init.rb'))
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/db/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
